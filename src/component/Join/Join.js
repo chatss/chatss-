2304e9e98c4fe81d070b885ec65950c940115ca5
Minsoo <zlemzlem5656@naver.com>
2020-02-05 20:39:30 +0900

    client : add / Join.js
    로그인 화면 : useState 를 사용해 두 개의 state를 선언해 주었습니다.
    
        1. name : 채팅에 참여할 이름
        2. room : 채팅방
    
    onChange 이벤트를 통해 입력한 이름과 방 정보를 setName,setRoom을 통해 state 세팅해주었습니다.
    
        onChange={
            (event)=>{
                setName(event.target.value)
            }
        }

--- src/component/Join/Join.js
+++ src/component/Join/Join.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import "./Join.css";
 
 const Join = () => {
-  return <div>Join</div>;
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+
+  return (
+    <div className="Join">
+      <div>
+        <h1>Join</h1>
+        <div>
+          <input
+            placeholder=""
+            type="text"
+            onChange={event => {
+              setName(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <input
+            placeholder=""
+            type="text"
+            onChange={event => {
+              setRoom(event.target.value);
+            }}
+          />
+        </div>
+        <Link
+          // 둘 중 하나라도 미입력시 제출 x
+          onClick={event => (!name || !room ? event.preventDefault() : null)}
+          to={`/chat/?name=${name}&room=${room}`}
+        >
+          <button type="submit">Sign in</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Join;
